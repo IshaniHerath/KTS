25cca4a386d6f9533f3698fd0b6a7845797c753a
Ishani <u.ishaniherath@gmail.com>
2020-05-15 09:51:14 +0530

    BE - userProfile and some APIs for Course

---
+++ server/course/courseRepository.js
@@ -0,0 +1,20 @@
+const pool = require('../connection');
+const userContext = {};
+
+/**
+ * getPrograms - Retrieve all programs
+ * @returns {array} list of Courses
+ */
+userContext.getPrograms = async () => {
+    try {
+        const allPrograms = await pool.query(
+            'select id, name from "program";'
+        );
+        return (allPrograms.rows);
+        // res.send (allUser.rows);
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+module.exports = userContext;
